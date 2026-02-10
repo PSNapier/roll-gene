97321793206636902e97161948a44a3ec0152e68
PSNapier <arjones.tx@gmail.com>
2026-02-10 16:15:28 -0600

    feat: brainstorming genetics logic

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -4,6 +4,37 @@ import AppLayout from '@/layouts/AppLayout.vue';
 import { dashboard } from '@/routes';
 import { type BreadcrumbItem } from '@/types';
 
+const singleLocus = 'singleLocus';
+const domRec = 'domRec';
+
+type GeneType = typeof singleLocus | typeof domRec;
+class Gene {
+    constructor(
+        public type: GeneType,
+        public alleles: string[],
+    ) {}
+}
+
+// gene type
+const dict = {
+    black: new Gene(singleLocus, ['E', 'e']),
+    agouti: new Gene(singleLocus, ['At', 'A', 'a']),
+    silver: new Gene(domRec, ['Z']),
+};
+
+const odds = {
+    realistic: {
+        roll1: 25,
+        roll2: 25,
+        roll3: 25,
+        roll4: 25,
+    },
+};
+
+console.log(dict);
+
+// pheno overrides
+
 const breadcrumbs: BreadcrumbItem[] = [
     {
         title: 'Dashboard',
@@ -15,6 +46,5 @@ const breadcrumbs: BreadcrumbItem[] = [
 <template>
     <Head title="Dashboard" />
 
-    <AppLayout :breadcrumbs="breadcrumbs">
-    </AppLayout>
+    <AppLayout :breadcrumbs="breadcrumbs"> </AppLayout>
 </template>
